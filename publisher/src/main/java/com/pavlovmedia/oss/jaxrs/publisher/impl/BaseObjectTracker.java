dca96b2f1ab2e4e46b162ad3311fd1df557161c4
Shawn Dempsay <sdempsay@pavlovmedia.com>
2017-06-22 22:12:01 -0500

    Back off the ReaderListener from the tracker

--- publisher/src/main/java/com/pavlovmedia/oss/jaxrs/publisher/impl/BaseObjectTracker.java
+++ publisher/src/main/java/com/pavlovmedia/oss/jaxrs/publisher/impl/BaseObjectTracker.java
@@ -26,8 +26,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.ext.Provider;
 
-import io.swagger.jaxrs.config.ReaderListener;
-
 /**
  * This is the skeleton for a service tracker that handles filtering
  * of objects, parsing them, and storing them.
@@ -150,7 +148,6 @@ public class BaseObjectTracker {
         // To be a jax target we can be a Feature, annotated with Path, or annotated with Provider
         return null != clazz.getDeclaredAnnotation(Path.class) 
                 || null != clazz.getDeclaredAnnotation(Provider.class)
-                || target instanceof Feature
-                || target instanceof ReaderListener; 
+                || target instanceof Feature;
     }
 }
