5738f33e7b250c39e58da3ea8aaf1ec44e4d32a6
Shawn Dempsay <sdempsay@pavlovmedia.com>
2018-07-09 07:57:45 -0500

    Updated the rebuild and repository

--- obr/rebuild.sh
+++ obr/rebuild.sh
@@ -1,3 +1,3 @@
 # Set this to where the tool jar is
-OBR_TOOL_JAR=~/buildspace/core/java/maven-obr-plugins/tool/target/dhc.maven.plugins.obr.tool-1.1.0-SNAPSHOT.jar
+OBR_TOOL_JAR=~/buildspace/core/java/maven-obr-plugins/tool/target/dhc.maven.plugins.obr.tool-2.1.0-SNAPSHOT.jar
 java -jar ${OBR_TOOL_JAR}  https://dl.bintray.com/pavlovmedia/pavlov-media-oss/com/pavlovmedia/oss/jaxrs/com.pavlovmedia.oss.jaxrs.publisher/ https://dl.bintray.com/pavlovmedia/pavlov-media-oss/com/pavlovmedia/oss/jaxrs/com.pavlovmedia.oss.jaxrs.webconsole/
